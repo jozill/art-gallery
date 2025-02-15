ebd3def02873bfb3a98be5f16f3811815f0d2d58
jozill <Jozilla13@gmail.com>
2025-02-15 03:51:50 -0500

    update server

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Test route
 app.get('/', (req, res) => {
-  res.send(`
-    <h1>Art Gallery</h1>
-    <p>Site is working!</p>
-    <a href="/admin">Admin Panel</a>
-  `);
+  res.send('Hello World');
 });
 
-// Simple admin route
-app.get('/admin', (req, res) => {
-  res.send(`
-    <h1>Admin Panel</h1>
-    <p>Admin page is working!</p>
-  `);
-});
-
-// Export for Vercel
 module.exports = app;
